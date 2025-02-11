33e652f9ac5255755a34307e1671d24403609b4b
Mathias-Evjen <143395521+Mathias-Evjen@users.noreply.github.com>
2025-02-11 11:21:16 +0100

    Opprettet spring boot prosjekt

---
+++ src/main/java/com/example/extraplassminilager/ExtraPlassMinilagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.extraplassminilager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraPlassMinilagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtraPlassMinilagerApplication.class, args);
+	}
+
+}
