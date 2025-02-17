23d7377f8fd88cc998a5976187fe73fc2b669586
TobiasFriis <143395113+TobiasAndreFriis@users.noreply.github.com>
2025-02-17 12:17:51 +0100

    printe ut boder i tabell

---
+++ src/main/resources/static/hentBoder.js
@@ -0,0 +1,20 @@
+function hentBoder() {
+    $.get("/hentAlleBoder", function(data){
+      printBoder(data);  
+    })
+}
+
+function testboder(){
+    let testboder = [{katNr: 1, areal: 10, volum: 9, antallLedig: 8}, {katNr: 2, areal: 8, volum: 6, antallLedig: 6}, {katNr: 3, areal: 11, volum: 12, antallLedig: 12}];
+    printBoder(testboder);
+}
+
+function printBoder(boder) {
+    for(bod in boder){
+        nybod = document.createElement("tr");
+        nybod.classList.add();
+        nybod.innerHTML = `<th>${bod.katNr}</th><th>${bod.areal}</th><th>${bod.volum}</th><th>${bod.antallLedig}</th>`;
+        document.getElementById("bodtabell").appendChild(nybod);
+    }
+
+}
