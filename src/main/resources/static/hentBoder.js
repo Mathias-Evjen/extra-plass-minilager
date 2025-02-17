280e18291023b01d9bda922a514ca7510ce54101
TobiasFriis <143395113+TobiasAndreFriis@users.noreply.github.com>
2025-02-17 15:00:22 +0100

    lagt til pris

--- src/main/resources/static/hentBoder.js
+++ src/main/resources/static/hentBoder.js
@@ -5,7 +5,7 @@ function hentBoder() {
 }
 
 function testboder(){
-    let testboder = [{katNr: 1, areal: 10, volum: 9, antallLedig: 8}, {katNr: 2, areal: 8, volum: 6, antallLedig: 6}, {katNr: 3, areal: 11, volum: 12, antallLedig: 12}];
+    let testboder = [{katNr: 1, areal: 10, volum: 9, antallLedig: 8, pris: 790}, {katNr: 2, areal: 8, volum: 6, antallLedig: 6, pris: 690}, {katNr: 3, areal: 11, volum: 12, antallLedig: 12, pris: 890}];
     printBoder(testboder);
 }
 
@@ -13,8 +13,8 @@ function printBoder(boder) {
     for(let i = 0; i < boder.length; i++){
         nybod = document.createElement("tr");
         nybod.classList.add();
-        nybod.innerHTML = `<th>${boder[i].katNr}</th><th>${boder[i].areal}</th><th>${boder[i].volum}</th><th>${boder[i].antallLedig}</th>`;
+        nybod.innerHTML = `<th>${boder[i].katNr}</th><th>${boder[i].areal}</th><th>${boder[i].volum}</th><th>${boder[i].antallLedig}</th><th>${boder[i].pris}kr`;
         document.getElementById("bodtabell").appendChild(nybod);
     }
-
+    
 }
