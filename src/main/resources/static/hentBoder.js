7134abf32cb11db4600b91ecb73ec8dff98f74b8
TobiasFriis <143395113+TobiasAndreFriis@users.noreply.github.com>
2025-02-17 15:50:10 +0100

    Design av UI

--- src/main/resources/static/hentBoder.js
+++ src/main/resources/static/hentBoder.js
@@ -13,7 +13,7 @@ function printBoder(boder) {
     for(let i = 0; i < boder.length; i++){
         nybod = document.createElement("tr");
         nybod.classList.add();
-        nybod.innerHTML = `<td>${boder[i].katNr}</td><td>${boder[i].areal}</td><td>${boder[i].volum}</td><td>${boder[i].antallLedig}</td>`;
+        nybod.innerHTML = `<td>${boder[i].katNr}</td><td>${boder[i].areal}m²</td><td>${boder[i].volum}m³</td><td>${boder[i].antallLedig}</td><td>${boder[i].pris}kr</td>`;
         document.getElementById("bodtabell").appendChild(nybod);
     }
     
