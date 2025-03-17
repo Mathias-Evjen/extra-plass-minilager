e549528bf709610e1827c5bbf68d110b134cd532
Olivereliasandersen <oland0229@oslomet.no>
2025-03-17 12:46:15 +0100

    Fikk fjernet knappen oppe/nede til å være synlig når du går inn på nettsiden

--- src/main/resources/static/tabellEllerKart.js
+++ src/main/resources/static/tabellEllerKart.js
@@ -1,34 +1,34 @@
-function tabellEllerKart(){
+function tabellEllerKart() {
     const checkboxTabellKart = document.getElementById("tabell-kart-checkbox");
     const checkboxOppeNede = document.getElementById("oppe-nede-checkbox");
+    const oppeNedeSwitch = document.querySelector(".switch-wrapper-oppe-nede");
     const kartOppe = document.getElementById("kart-oppe-div");
     const kartNede = document.getElementById("kart-nede-div");
     const tabell = document.getElementById("tabell");
     const kartDiv = document.getElementById("kart-div");
     const kartContainer = document.getElementById("kart-container");
+
     if (!checkboxTabellKart.classList.contains("active")) {
         tabell.style.display = "block";
-        kartOppe.style.display = "none";
-        kartNede.style.display = "none";
         kartDiv.style.display = "none";
         kartContainer.style.display = "none";
-    }
-    else if (!checkboxOppeNede.classList.contains("active") && checkboxTabellKart.classList.contains("active")) {
-        tabell.style.display = "none";
-        kartOppe.style.display = "block";
-        kartNede.style.display = "none";
-        kartDiv.style.display = "block";
-        kartContainer.style.display = "block";
-        kartContainer.style.height = "678.5px";
-    }
-    else {
-        console.log(checkboxOppeNede.classList.contains("active"))
-        tabell.style.display = "none";
         kartOppe.style.display = "none";
-        kartNede.style.display = "block";
+        kartNede.style.display = "none";
+        oppeNedeSwitch.style.display = "none"; 
+    } else {
+        tabell.style.display = "none";
         kartDiv.style.display = "block";
         kartContainer.style.display = "block";
-        kartContainer.style.height = "571.1px";
+        oppeNedeSwitch.style.display = "flex"; 
+
+        if (!checkboxOppeNede.classList.contains("active")) {
+            kartOppe.style.display = "block";
+            kartNede.style.display = "none";
+            kartContainer.style.height = "678.5px";
+        } else {
+            kartOppe.style.display = "none";
+            kartNede.style.display = "block";
+            kartContainer.style.height = "571.1px";
+        }
     }
-    
 }
