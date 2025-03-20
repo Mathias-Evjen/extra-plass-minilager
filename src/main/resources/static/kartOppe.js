e47bbdaece3a283ed44e1c53cb1c8a90b2b2b2f8
Mathias-Evjen <143395521+Mathias-Evjen@users.noreply.github.com>
2025-03-20 14:12:20 +0100

    La til trapp-ikon på kartet oppe og reposisjonerte ikonet for kartet nede

--- src/main/resources/static/kartOppe.js
+++ src/main/resources/static/kartOppe.js
@@ -1131,9 +1131,9 @@ export function printKartOppe() {
                 <!-- Trapp -->
                 <svg x="0" y="0">
                     <rect width="100%" height="100%" fill="beige"/>
+                    <svg id="trapp-ikon" xmlns="http://www.w3.org/2000/svg" x="0.5%" y="50%" height="48px" viewBox="0 0 24 24" width="48px" fill="#000000"><g><rect fill="none" height="24" width="24" x="0"/><g><g><path d="M19,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V5C21,3.9,20.1,3,19,3z M18,8h-2.42v3.33H13v3.33h-2.58 V18H6v-2h2.42v-3.33H11V9.33h2.58V6H18V8z"/></g></g></g></svg>
                     <line x1="0" x2="0" y1="0" y2="100%" stroke="black" stroke-width="2"/>
                     <line x1="0" x2="100%" y1="100%" y2="100%" stroke="black" stroke-width="2"/>
-                    <text x="6rem" y="80%">Trapp ---></text>
                 </svg>
 
                 <!-- Klasse 1 -->
